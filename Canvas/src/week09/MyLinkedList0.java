f01f1eff7a7706f5800acbeef08211ef7fbdc3e1
dlqja <beomjin754@gmail.com>
2025-05-09 10:19:58 +0900

    [Feat] week10

--- Canvas/src/week09/MyLinkedList0.java
+++ Canvas/src/week09/MyLinkedList0.java
@@ -3,40 +3,46 @@ package week09;
 import week07.MyArrayList2;
 
 public class MyLinkedList0 {
-	
-	class Node{
-	int data;
-	Node next;
-	
-	Node(int d){
-		data =d;
-		next = null;
-	}
-	
-	public String toSrting() {
-		return ""+data;
+
+	class Node {
+		int data;
+		Node next;
+
+		Node(int d) {
+			data = d;
+			next = null;
+		}
+
+		public String toSrting() {
+			return "" + data;
 		}
 	}
+
 	Node head;
+	int size;
+
 	public MyLinkedList0() {
 		head = null;
+		size = 0;
 	}
-	
+
 	public boolean isEmpty() {
-		return (head==null);
+		return (head == null);
 	}
-	
+
 	public void add(int value) {
 		addFirst(value);
-		addLast(value);	}
-	
+		addLast(value);
+	}
+
 	private void addLast(int value) {
-		if(isEmpty()) addFirst(value);
+		if (isEmpty())
+			addFirst(value);
 		else {
 			Node newNode = new Node(value);
 			Node p = head;
-			while(p.next!=null) {
-				p=p.next;
+			while (p.next != null) {
+				p = p.next;
 			}
 			p.next = newNode;
 		}
@@ -46,65 +52,142 @@ public class MyLinkedList0 {
 		Node newNode = new Node(value);
 		newNode.next = head;
 		head = newNode;
+		size++;
 	}
-	
+
 	public int indexOf(int value) {
-		int index =0;
+		int index = 0;
 		Node p = head;
-		while(p!=null) {
-			if(p.data==value)return ++index;
-			else {index++; p=p.next;}
+		while (p != null) {
+			if (p.data == value)
+				return ++index;
+			else {
+				index++;
+				p = p.next;
+			}
 		}
 		return -1;
 	}
-	
-	public void add(int index,int value) { //0<=index <=size()
-		if(checkInndexRange(index)) {
-			if(index==0) addFirst(value);
-			
-			Node newNode = new Node(value);
-			int i=1;
-			Node p = head;
-			while(p.next !=null) {
-				if(i==index) {
-					newNode.next = p.next;
-					p.next = newNode;
-					return; 
+
+	public void add(int index, int value) { // 0<=index <=size()
+		if (checkInndexRange(index)) {
+			if (index == 0)
+				addFirst(value);
+			else {
+				Node newNode = new Node(value);
+				int i = 1;
+				Node p = head;
+				while (p.next != null) {
+					if (i == index) {
+						newNode.next = p.next;
+						p.next = newNode;
+						size++;
+						return;
+						
+					} else {
+						i++;
+						p = p.next;
+					}
 				}
-				else {
-					i++;
-					p=p.next;
-				}
+
 			}
-		}else if(index==size()) {
+		} else if (index == size()) {
 			addLast(value);
 		}
 	}
-	
-	private int size() {
-		int n=0;
-		Node p = head;
-		while(p!=null) {
-		n++;
-		p=p.next;
-		}
-		return n;
-	}	
 
-	private boolean checkInndexRange(int index) {
-		if(index>=0&&index<=size()) return true;
-		else return false;
+	private int size() {
+//		int n = 0;
+//		Node p = head;
+//		while (p != null) {
+//			n++;
+//			p = p.next;
+//		}
+//		return n;
+		return size;
 	}
 	
-	public String toString() {
-		String str ="";
-		Node p =head;
-		while(p!=null) {
-			str = str+p.data+" ";
+	public void clear() {
+		head=null;
+	}
+	
+	public boolean contains(int value) {
+		return (indexOf(value)!=-1);
+	}
+
+	public int get(int idx) {
+		int i =0;
+		Node p = head;
+		while(p !=null) {
+			if(i==idx) return p.data;
+			i++;
 			p=p.next;
+		}
+		return -999;
+	}
+	
+	public void set(int idx, int value) {
+		int i =0;
+		Node p = head;
+		while(p !=null) {
+			if(i==idx) p.data=value;
+			i++;
+			p=p.next;
+		}
+		//error massage
+	}
+	
+	public int remove(int idx) { // return the value
+		int ret = -9999;
+		if(checkInndexRange(idx)) {
+			if(idx==0) {
+				ret = removeFirst();
+			}
+			else {
+				int i=1;
+				Node p = head;
+				Node q = p.next;
+				while( q!=null) {
+					if(i==idx) {
+						ret = q.data;
+						p.next=q.next;
+						break;
+					}
+					i++;
+					p=q;
+					q=q.next;
+				}
+			}
+		}
+		return ret;
+	}
+	
+	private int removeFirst() {
+		int ret = -999;
+		if(head!=null) {
+			ret = head.data;
+			head = head.next;
+		}
+		return ret;
+	}
+
+	private boolean checkInndexRange(int index) {
+		if (index >= 0 && index <= size())
+			return true;
+		else
+			return false;
+	}
+
+	public String toString() {
+		String str = "";
+		Node p = head;
+		while (p != null) {
+			str = str + p.data + " ";
+			p = p.next;
 		}
 		return str;
 	}
+
 
 	public static void main(String[] args) {
 
@@ -119,5 +202,15 @@ public class MyLinkedList0 {
 		for (int i = 4; i < 10; i++)
 			list.add(data[i]); // cf : list[i]=data[i]
 		list.toString();
-		}
+		list.remove(74);
+		System.out.println("After removing 74: " + list.toString());
+		
+		list.remove((Integer) 336);
+
+		list.add(3, 111);
+		list.add(list.size(), 222);
+
+		list.clear();
+
+	}
 }
---
+++ Canvas/src/week10/MyLinkedList1.java
@@ -0,0 +1,199 @@
+public class MyLinkedList1 {
+	class Node {
+		String data;
+		Node next;
+
+		Node(String d) {
+			data = d;
+			next = null;
+		}
+
+		public String toString() {
+			return "" + data;
+		}
+	}
+
+	Node head;
+	int size;
+
+	public MyLinkedList1() {
+		head = null;
+		size = 0;
+	}
+
+	public boolean isEmpty() {
+		return (head == null);
+	}
+
+	public void add(String value) {
+		addFirst(value);
+		addLast(value);
+	}
+
+	private void addLast(String value) {
+		if (isEmpty())
+			addFirst(value);
+		else {
+			Node newNode = new Node(value);
+			Node p = head;
+			while (p.next != null) {
+				p = p.next;
+			}
+			p.next = newNode;
+		}
+	}
+
+	private void addFirst(String value) {
+		Node newNode = new Node(value);
+		newNode.next = head;
+		head = newNode;
+		size++;
+	}
+
+	public int indexOf(String value) {
+		int index = 0;
+		Node p = head;
+		while (p != null) {
+			if (p.data.equals(value))
+				return index;
+			else {
+				index++;
+				p = p.next;
+			}
+		}
+		return -1;
+	}
+
+	public void add(int index, String value) {
+		if (checkInndexRange(index)) {
+			if (index == 0)
+				addFirst(value);
+			else {
+				Node newNode = new Node(value);
+				int i = 1;
+				Node p = head;
+				while (p.next != null) {
+					if (i == index) {
+						newNode.next = p.next;
+						p.next = newNode;
+						size++;
+						return;
+					} else {
+						i++;
+						p = p.next;
+					}
+				}
+			}
+		} else if (index == size()) {
+			addLast(value);
+		}
+	}
+
+	private int size() {
+		return size;
+	}
+
+	public void clear() {
+		head = null;
+	}
+
+	public boolean contains(String value) {
+		return (indexOf(value) != -1);
+	}
+
+	public String get(int idx) {
+		int i = 0;
+		Node p = head;
+		while (p != null) {
+			if (i == idx)
+				return p.data;
+			i++;
+			p = p.next;
+		}
+		return null;
+	}
+
+	public void set(int idx, String value) {
+		int i = 0;
+		Node p = head;
+		while (p != null) {
+			if (i == idx)
+				p.data = value;
+			i++;
+			p = p.next;
+		}
+	}
+
+	public String remove(int idx) {
+		String ret = null;
+		if (checkInndexRange(idx)) {
+			if (idx == 0) {
+				ret = removeFirst();
+			} else {
+				int i = 1;
+				Node p = head;
+				Node q = p.next;
+				while (q != null) {
+					if (i == idx) {
+						ret = q.data;
+						p.next = q.next;
+						size--;
+						break;
+					}
+					i++;
+					p = q;
+					q = q.next;
+				}
+			}
+		}
+		return ret;
+	}
+
+	public String remove(Object value) {
+		if (!(value instanceof String)) return null;
+		String val = (String) value;
+
+		if (head != null) {
+			if (head.data.equals(val)) {
+				return removeFirst();
+			} else {
+				Node p = head;
+				Node q = p.next;
+				while (q != null) {
+					if (q.data.equals(val)) {
+						p.next = q.next;
+						size--;
+						return q.data;
+					}
+					p = q;
+					q = p.next;
+				}
+			}
+		}
+		return null;
+	}
+
+	private String removeFirst() {
+		String ret = null;
+		if (head != null) {
+			ret = head.data;
+			head = head.next;
+			size--;
+		}
+		return ret;
+	}
+
+	private boolean checkInndexRange(int index) {
+		return (index >= 0 && index <= size());
+	}
+
+	public String toString() {
+		String str = "";
+		Node p = head;
+		while (p != null) {
+			str = str + p.data + " ";
+			p = p.next;
+		}
+		return str.trim();
+	}
+}
