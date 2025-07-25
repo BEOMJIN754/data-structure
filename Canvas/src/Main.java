e1e9f6c0272f4852d6a1149254e84d400b85789d
dlqja <beomjin754@gmail.com>
2025-07-25 21:50:52 +0900

    [Feat] Print queue

--- Canvas/src/Main.java
+++ Canvas/src/Main.java
@@ -1,43 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int N, cnt;
-    static int[] chess;
- 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        chess = new int[N];
- 
-        dfs(0);
-        System.out.println(cnt);
-    }
- 
-    private static void dfs(int col) {
-        if (col == N) {
-            cnt++;
-            return;
-        }
- 
-        for (int i = 0; i < N; i++) {
-            chess[col] = i;
-            if (isOk(col)) {
-                dfs(col + 1);
-            }
-        }
-    }
- 
-    private static boolean isOk(int col) {
-        for (int i = 0; i < col; i++) {
-            if (chess[col] == chess[i])
-                return false;
- 
-            if (col - i == Math.abs(chess[col] - chess[i]))
-                return false;
-        }
- 
-        return true;
-    }
+
+	public Queue<Integer> queue1;
+	public Queue<Integer> queue2;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int size = Integer.parseInt(st.nextToken());
+			int target = Integer.parseInt(st.nextToken());
+			for(int j=0;j<size;j++) {
+				
+			}
+		}
+
+	}
 }
